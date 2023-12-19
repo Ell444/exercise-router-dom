6ba1688ac3a33638b115f387e43d503a20ced038
Laura Sirigu <laurasirigu43@gmail.com>
2023-12-19 17:59:05 +0100

    feat || exercise completed with bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
+
+import { Link, NavLink, Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import Countries from './components/Countries'
+import NotFound from './components/NotFound'
+import CountryPage from './components/CountryPage'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [selectValue, setSelectValue] = useState('en')
+  const lang = {
+    "en": { about: "About Us", countries: "Countries" },
+    "it": { about: "Chi siamo", countries: "Paesi" }
+  }
+
+  return (<>
+    <nav className='nav-container'>
+      <menu className='app-menu'>
+        <li>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink className='nav-link' to='about'>{lang[selectValue].about}</NavLink>
+        </li>
+        <li>
+          <NavLink className='nav-link' to='countries'>{lang[selectValue].countries}</NavLink>
+        </li>
+        <li>
+          <select
+            value={selectValue}
+            onChange={e => setSelectValue(e.target.value)}>
+            <option value="en">EN</option>
+            <option value="it">IT</option>
+          </select>
+        </li>
+      </menu>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='about' element={<AboutUs language={selectValue} />} />
+      <Route path='countries'>
+        <Route index element={<Countries language={selectValue} />} />
+        <Route path='/countries/:code' element={<CountryPage language={selectValue} />} />
+      </Route>
+      <Route path='*' element={<NotFound language={selectValue} />} />
+
+    </Routes>
+
+
+
+  </>)
 }
 
 export default App
